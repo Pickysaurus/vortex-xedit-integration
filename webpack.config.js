7b641c8e3a2172eb66ef3ba5b58dc5b734fc6ae2
Mike Watling <mike.watling@nexusmods.com>
2023-03-23 09:17:58 +0000

    Updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
 let webpack = require('vortex-api/bin/webpack').default;
 
-module.exports = webpack('xedit-integration', __dirname, 4);
+module.exports = webpack('xedit-integration', __dirname, 5);
 
